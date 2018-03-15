2f4aa54e093a065bcbf0d1263b96ce43b83784f6
B <597895752@qq.com>
2018-03-15 23:33:23 +0800

    homework

--- src/common/js/cache.js
+++ src/common/js/cache.js
@@ -6,6 +6,9 @@ const SEARCH_MAX_LENGTH = 15
 const PLAY_KEY = '__play__'
 const PLAY_MAX_LENGTH = 200
 
+const FAVORITE_KEY = '__favorite__'
+const FAVORITE_MAX_LEN = 200
+
 function insertArray(arr, val, compare, maxLen) {
   const index = arr.findIndex(compare)
   if (index === 0) {
@@ -60,5 +63,23 @@ export function savePlay(song) {
     return item.id === song.id
   }, PLAY_MAX_LENGTH)
   storage.set(PLAY_KEY, songs)
+  return songs
+}
+
+export function saveFavorite(song) {
+  let songs = storage.get(FAVORITE_KEY, [])
+  insertArray(songs, song, (item) => {
+    return item.id === song.id
+  }, FAVORITE_MAX_LEN)
+  storage.set(FAVORITE_KEY, songs)
+  return songs
+}
+
+export function deleteFavorite(song) {
+  let songs = storage.get(FAVORITE_KEY, [])
+  deleteArray(songs, (item) => {
+    return item.id === song.id
+  })
+  storage.set(FAVORITE_KEY, songs)
   return songs
 }
